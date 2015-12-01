6738b81490ab867e4f7a9b17eae4954298cdb7c6
resteve <resteve@zikzakmedia.com>
2015-12-01 11:04:14 +0100

    Tests

--- tests/test_carrier_note.py
+++ tests/test_carrier_note.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the carrier_note module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierNoteTestCase(unittest.TestCase):
+class CarrierNoteTestCase(ModuleTestCase):
     'Test Carrier Note module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_note')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('carrier_note')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'carrier_note'
 
 
 def suite():
