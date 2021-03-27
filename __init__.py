710beeea8a76e6ccaa78c4e5a8c0b8c028690b99
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-27 23:10:35 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .carrier import *
+from . import carrier
 
 def register():
     Pool.register(
-        Carrier,
+        carrier.Carrier,
         module='carrier_note', type_='model')
